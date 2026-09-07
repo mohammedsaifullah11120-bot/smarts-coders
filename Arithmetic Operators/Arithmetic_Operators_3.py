c3e381d2569279e0a6b2bdc223a78c4bbfcca61a
mohammed <mohammedsaifullah11120@gmail.com>
2026-09-07 09:03:00 +0000

    arthemetic operation

--- Arithmetic Operators/Arithmetic_Operators_3.py
+++ Arithmetic Operators/Arithmetic_Operators_3.py
@@ -1,1 +1,9 @@
 		# 3	Calculate simple interest given P, R, T → (P × R × T) / 100	Resource Link
+
+p=float(input( "The starting amount of money borrowed or invested"))
+r=float(input("the yearly interest rate"))
+t=float(input("The duration of the loan"))
+
+s=p*r*t/100
+
+print("the simple intrest is ",s)
